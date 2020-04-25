32ed7d03fd3f2101a5e3029cc30de8b0a91d872a
Sebastian Gonzalez <s.gonzalez.calquin@gmail.com>
2020-04-24 20:48:58 -0400

    if y else

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('Hola Mundo');
+var nombre = "Sebastian";
+var nombre = 2;
+
+var numero = 12 + 12;
+
+const mivariable = "Alis";
+
+let mivariable2 = "Alis";
+
+if(mivariable === "Alis"){
+    console.log("La variables es Alis");
+}else{
+    console.log("La variable no se llama Alis");
+}
